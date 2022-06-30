eeca21f948b3cf0df1221c391fd492ee200c2dbe
weiran1999 <907221539@qq.com>
2022-06-30 16:37:27 +0800

    style: delete a useless dependence

--- cloud-mission/src/main/java/com/weiran/mission/controller/CloudController.java
+++ cloud-mission/src/main/java/com/weiran/mission/controller/CloudController.java
@@ -14,7 +14,6 @@ import com.weiran.mission.service.WelcomeService;
 import com.weiran.mission.utils.qiniu.ImageKit;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
