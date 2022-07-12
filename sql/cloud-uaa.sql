f5709aa9cef1b3a8ed5c4917c6c96452178a19ac
weiran1999 <907221539@qq.com>
2022-07-12 09:30:07 +0800

    chore: alter the number

--- sql/cloud-uaa.sql
+++ sql/cloud-uaa.sql
@@ -37,8 +37,8 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (1, '孙念', '18077200000', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302200010300531', '2022-02-26 16:22:19', '2022-03-25 20:59:09', '2022-05-24 20:36:30');
-INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (2, 'sun', '18077200001', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302199910310531', '2022-03-15 11:40:17', '2022-03-25 20:51:07', '2022-03-21 02:36:03');
+INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (1, '孙念', '13870035320', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302200010300531', '2022-02-26 16:22:19', '2022-03-25 20:59:09', '2022-05-24 20:36:30');
+INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (2, 'sun', '13870035321', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302199910310531', '2022-03-15 11:40:17', '2022-03-25 20:51:07', '2022-03-21 02:36:03');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
