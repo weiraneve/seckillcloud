0d2684003345bcd132a826b6123a8b822566665c
weiran1999 <907221539@qq.com>
2023-01-17 22:09:24 +0800

    feat: add init.sql and update application & docker file

---
+++ sql/init/init.sql
@@ -0,0 +1,6 @@
+-- 创建数据库
+CREATE DATABASE `cloud-uaa` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `cloud-manage` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `cloud-mission-goods` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `cloud-mission-seckillGoods` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `cloud-mission-order` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
