590f07f0804e9f7b1245c2996c3c5fd251b17a3d
weiran <907221539@qq.com>
2023-07-15 13:24:06 +0800

    refactor: refactor RedisLua to reduce redundancy and improve readability

--- cloud-common/src/main/java/com/weiran/common/redis/manager/RedisLua.java
+++ cloud-common/src/main/java/com/weiran/common/redis/manager/RedisLua.java
@@ -17,56 +17,43 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class RedisLua {
 
+    private static final String STOCK_SCRIPT = "local stock = tonumber(redis.call('get',KEYS[1]));" +
+            "if (stock <= 0) then" +
+            "    return -1;" +
+            "    end;" +
+            "if (stock > 0) then" +
+            "    return redis.call('incrby', KEYS[1], -1);" +
+            "    end;";
+
+    private static final String COUNT_SCRIPT =
+            "local num=tonumber(redis.call('get',KEYS[1]));" +
+                    "return num;";
+
+    private static final String ADD_SCRIPT =
+            "local num=redis.call('incr',KEYS[1]) return num";
+
     final RedisTemplate<String, Object> redisTemplate;
 
-    /**
-     * 判断库存和预减库存
-     */
-    public Long judgeStockAndDecrStock(Long goodsId) {
-        String stockScript = "local stock = tonumber(redis.call('get',KEYS[1]));" +
-                "if (stock <= 0) then" +
-                "    return -1;" +
-                "    end;" +
-                "if (stock > 0) then" +
-                "    return redis.call('incrby', KEYS[1], -1);" +
-                "    end;";
-
-        DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>(stockScript, Long.class);
-        return redisTemplate.execute(redisScript, Collections.singletonList(RedisConstant.SECKILL_KEY + goodsId));
-
-    }
-
-    /**
-     * 统计访问次数
-     */
-    public Long getVisitorCount(String lockKey) {
+    public Long executeScript(String script, String key) {
         try {
-            // LUA脚本中 tonumber函数可将调用结果转换为number形式
-            String countScript =
-                    "local num=tonumber(redis.call('get',KEYS[1]));" +
-                            "return num;";
-            DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>(countScript, Long.class);
-            return redisTemplate.execute(redisScript, Collections.singletonList(lockKey));
+            DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
+            return redisTemplate.execute(redisScript, Collections.singletonList(key));
         } catch (Exception e) {
-            log.error("统计访问次数失败！！！");
+            log.error("Script execution failed: " + script);
             log.error(e.toString());
             return null;
         }
     }
 
-    /**
-     * 增加访问次数
-     */
+    public Long judgeStockAndDecrStock(Long goodsId) {
+        return executeScript(STOCK_SCRIPT, RedisConstant.SECKILL_KEY + goodsId);
+    }
+
+    public Long getVisitorCount(String lockKey) {
+        return executeScript(COUNT_SCRIPT, lockKey);
+    }
+
     public void addVisitorCount(String lockKey) {
-        try {
-            String addScript =
-                    "local num=redis.call('incr',KEYS[1]) return num";
-            DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>(addScript, Long.class);
-            // 限制60s访问5次
-            redisTemplate.execute(redisScript, Collections.singletonList(lockKey));
-        } catch (Exception e) {
-            log.error("增加访问次数失败！！！");
-            log.error(e.toString());
-        }
+        executeScript(ADD_SCRIPT, lockKey);
     }
 }
