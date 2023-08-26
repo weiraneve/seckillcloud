e2b0e1a285deed59a95c0f1cbd7125f28862f3c4
weiraneve <907221539@qq.com>
2023-08-26 17:10:49 +0800

    refactor: refactor treeHelper to streamline node conversion

--- cloud-manage/src/main/java/com/weiran/manage/utils/TreeHelper.java
+++ cloud-manage/src/main/java/com/weiran/manage/utils/TreeHelper.java
@@ -1,77 +1,71 @@
 package com.weiran.manage.utils;
 
-
 import com.weiran.manage.dto.PermissionMenuDTO;
 import com.weiran.manage.dto.TreeRoleMenuDTO;
-import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-
 public class TreeHelper {
 
-    public static List<PermissionMenuDTO> getSortedNodes(List<PermissionMenuDTO> datas) {
-        List<PermissionMenuDTO> nodes = convertData2Node(datas);
-        // 拿到根节点
-        return getRootNodes(nodes);
+    public static List<PermissionMenuDTO> getSortedNodes(List<PermissionMenuDTO> dtoList) {
+        convertData2Node(dtoList, (n, m) -> Objects.equals(m.getParentId(), n.getId()));
+        return findRootNodes(dtoList, node -> node.getParentId() == null || node.getParentId() == 0);
     }
 
-    private static List<PermissionMenuDTO> convertData2Node(List<PermissionMenuDTO> nodes) {
+    public static List<TreeRoleMenuDTO> getSortedTreeNodes(List<TreeRoleMenuDTO> dtoList) {
+        convertData2TreeNode(dtoList, (n, m) -> Objects.equals(m.getParentId(), n.getValue()));
+        return findRootNodes(dtoList, node -> node.getParentId() == null || node.getParentId() == 0);
+    }
+
+    private static void convertData2Node(List<PermissionMenuDTO> nodes, NodeRelation<PermissionMenuDTO> checker) {
         for (int i = 0; i < nodes.size(); i++) {
             PermissionMenuDTO n = nodes.get(i);
             for (int j = i + 1; j < nodes.size(); j++) {
                 PermissionMenuDTO m = nodes.get(j);
-                if (Objects.equals(m.getParentId(), n.getId())) {
-                    if (StringUtils.isEmpty(n.getChildren())) {
+                if (checker.isParent(n, m)) {
+                    if (n.getChildren() == null) {
                         n.setChildren(new ArrayList<>());
                     }
                     n.getChildren().add(m);
                 }
             }
         }
-        return nodes;
     }
 
-    private static List<PermissionMenuDTO> getRootNodes(List<PermissionMenuDTO> nodes) {
-        List<PermissionMenuDTO> root = new ArrayList<>();
-        for (PermissionMenuDTO node : nodes) {
-            if (node.getParentId() == null || node.getParentId() == 0) {
-                root.add(node);
-            }
-        }
-        return root;
-    }
-
-    public static List<TreeRoleMenuDTO> getSortedTreeNodes(List<TreeRoleMenuDTO> datas) {
-        List<TreeRoleMenuDTO> nodes = convertData2TreeNode(datas);
-        // 拿到根节点
-        return getTreeRootNodes(nodes);
-    }
-
-    private static List<TreeRoleMenuDTO> convertData2TreeNode(List<TreeRoleMenuDTO> nodes) {
+    private static void convertData2TreeNode(List<TreeRoleMenuDTO> nodes, NodeRelation<TreeRoleMenuDTO> checker) {
         for (int i = 0; i < nodes.size(); i++) {
             TreeRoleMenuDTO n = nodes.get(i);
             for (int j = i + 1; j < nodes.size(); j++) {
                 TreeRoleMenuDTO m = nodes.get(j);
-                if (Objects.equals(m.getParentId(), n.getValue())) {
-                    if (StringUtils.isEmpty(n.getChildren())) {
+                if (checker.isParent(n, m)) {
+                    if (n.getChildren() == null) {
                         n.setChildren(new ArrayList<>());
                     }
                     n.getChildren().add(m);
                 }
             }
         }
-        return nodes;
     }
-    private static List<TreeRoleMenuDTO> getTreeRootNodes(List<TreeRoleMenuDTO> nodes) {
-        List<TreeRoleMenuDTO> root = new ArrayList<>();
-        for (TreeRoleMenuDTO node : nodes) {
-            if (node.getParentId() == null || node.getParentId() == 0) {
+
+    private static <T> List<T> findRootNodes(List<T> nodes, NodeChecker<T> checker) {
+        List<T> root = new ArrayList<>();
+        for (T node : nodes) {
+            if (checker.isRoot(node)) {
                 root.add(node);
             }
         }
         return root;
+    }
+
+    @FunctionalInterface
+    private interface NodeRelation<T> {
+        boolean isParent(T node1, T node2);
+    }
+
+    @FunctionalInterface
+    private interface NodeChecker<T> {
+        boolean isRoot(T node);
     }
 }
