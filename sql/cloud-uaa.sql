5a7e628606070d35cdad5d5f3043969968646e39
weiraneve <907221539@qq.com>
2023-09-03 22:31:54 +0800

    feat: add uaa demo sql

--- sql/cloud-uaa.sql
+++ sql/cloud-uaa.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 30/05/2022 21:28:35
+ Date: 03/09/2023 21:28:35
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,7 @@ CREATE TABLE `user` (
 BEGIN;
 INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (1, '孙念', '13870035320', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302200010300531', '2022-02-26 16:22:19', '2022-03-25 20:59:09', '2022-05-24 20:36:30');
 INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (2, 'sun', '13870035321', '9508534c15e39a3f5a586aec9be941f6c249e646e06857ea0c3b0c33545dc679', '510302199910310531', '2022-03-15 11:40:17', '2022-03-25 20:51:07', '2022-03-21 02:36:03');
+INSERT INTO `user` (`id`, `user_name`, `phone`, `password`, `identity_card_id`, `created_at`, `updated_at`, `last_login_time`) VALUES (3, 'demo', '12345678910', '123', '510302199910310531', '2022-03-15 11:40:17', '2022-03-25 20:51:07', '2022-03-21 02:36:03');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
