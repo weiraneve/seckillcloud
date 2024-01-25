1b00e1216c92a99403b800f47461d720293f9134
weiraneve <907221539@qq.com>
2024-01-25 19:17:52 +0800

    refactor: add swagger controller api

--- cloud-manage/src/main/java/com/weiran/manage/controller/web/WelcomeController.java
+++ cloud-manage/src/main/java/com/weiran/manage/controller/web/WelcomeController.java
@@ -3,6 +3,7 @@ package com.weiran.manage.controller.web;
 import com.weiran.common.obj.Result;
 import com.weiran.common.pojo.vo.WelcomeVO;
 import com.weiran.manage.cloud.MissionClient;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,8 @@ public class WelcomeController {
 
     private final MissionClient missionClient;
 
-    /**
-     * 统计
-     */
     @GetMapping
+    @ApiOperation("统计数据")
     public Result<WelcomeVO> welcome() {
         return missionClient.welcome();
     }
