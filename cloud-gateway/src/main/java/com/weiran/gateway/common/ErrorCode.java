ff9290b5d4b77d7944453cc62be2fc954a629178
weiraneve <907221539@qq.com>
2024-03-01 17:45:01 +0800

    refactor: update micro service unavailable error code

--- cloud-gateway/src/main/java/com/weiran/gateway/common/ErrorCode.java
+++ cloud-gateway/src/main/java/com/weiran/gateway/common/ErrorCode.java
@@ -7,8 +7,8 @@ public class ErrorCode {
     public static final int UNAUTHORIZED = 401;
     public static final int FORBIDDEN = 403;
     public static final int NOT_FOUNT = 404;
-    public static final int MICRO_SERVICE_UNAVAILABLE = 400;
     public static final int SERVER_ERROR = 500;
     public static final int BAD_GATEWAY = 502;
     public static final int SERVICE_UNAVAILABLE = 503;
+    public static final int MICRO_SERVICE_UNAVAILABLE = 503;
 }
