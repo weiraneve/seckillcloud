28520cda9a18ab298ffd2ac2a286a8d49d9da6d1
weiraneve <907221539@qq.com>
2024-04-29 18:47:20 +0800

    refactor: remove useless code and use try catch for handler

--- cloud-manage/src/main/java/com/weiran/manage/config/security/JsonLoginSuccessHandler.java
+++ cloud-manage/src/main/java/com/weiran/manage/config/security/JsonLoginSuccessHandler.java
@@ -3,6 +3,7 @@ package com.weiran.manage.config.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weiran.common.obj.Result;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -14,6 +15,7 @@ import java.io.IOException;
 /**
  * 自定义的用户成功登录处理
  */
+@Slf4j
 @RequiredArgsConstructor
 public class JsonLoginSuccessHandler implements AuthenticationSuccessHandler {
 
@@ -22,10 +24,14 @@ public class JsonLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        String token = jwtUserService.saveUserLoginInfo((UserDetails) authentication.getPrincipal());
-        response.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(new ObjectMapper().writeValueAsString(Result.success(token)));
+        try {
+            String token = jwtUserService.saveUserLoginInfo((UserDetails) authentication.getPrincipal());
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(Result.success(token)));
+        } catch (Exception e) {
+            log.error("处理认证成功异常", e);
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "内部服务器错误");
+        }
     }
 }
