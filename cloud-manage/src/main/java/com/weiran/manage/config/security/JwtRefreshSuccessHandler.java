852e47896a37758945384184e30b5203c1151a5d
weiraneve <907221539@qq.com>
2024-06-02 13:56:11 +0800

    refactor: format code

--- cloud-manage/src/main/java/com/weiran/manage/config/security/JwtRefreshSuccessHandler.java
+++ cloud-manage/src/main/java/com/weiran/manage/config/security/JwtRefreshSuccessHandler.java
@@ -18,25 +18,25 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtRefreshSuccessHandler implements AuthenticationSuccessHandler {
 
-	// 刷新间隔5分钟
-	private static final int TOKEN_REFRESH_INTERVAL = 300;
+    // 刷新间隔5分钟
+    private static final int TOKEN_REFRESH_INTERVAL = 300;
 
-	private static final String RESPONSE_HEADER = "Authorization";
+    private static final String RESPONSE_HEADER = "Authorization";
 
-	private final JwtUserService jwtUserService;
+    private final JwtUserService jwtUserService;
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) {
-		DecodedJWT jwt = ((JwtAuthenticationToken)authentication).getToken();
-		boolean shouldRefresh = shouldTokenRefresh(jwt.getIssuedAt());
-		if(shouldRefresh) {
-            String newToken = jwtUserService.saveUserLoginInfo((UserDetails)authentication.getPrincipal());
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        DecodedJWT jwt = ((JwtAuthenticationToken) authentication).getToken();
+        boolean shouldRefresh = shouldTokenRefresh(jwt.getIssuedAt());
+        if (shouldRefresh) {
+            String newToken = jwtUserService.saveUserLoginInfo((UserDetails) authentication.getPrincipal());
             response.setHeader(RESPONSE_HEADER, newToken);
         }
-	}
+    }
 
-	private boolean shouldTokenRefresh(Date issueAt) {
+    private boolean shouldTokenRefresh(Date issueAt) {
         LocalDateTime issueTime = LocalDateTime.ofInstant(issueAt.toInstant(), ZoneId.systemDefault());
         return LocalDateTime.now().minusSeconds(TOKEN_REFRESH_INTERVAL).isAfter(issueTime);
     }
