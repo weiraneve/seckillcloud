b0bfa324297273d655f1fa6b9c9e6c0c790773c9
weiraneve <907221539@qq.com>
2024-06-02 13:56:01 +0800

    refactor: extract response header and update constant

--- cloud-manage/src/main/java/com/weiran/manage/config/security/JwtRefreshSuccessHandler.java
+++ cloud-manage/src/main/java/com/weiran/manage/config/security/JwtRefreshSuccessHandler.java
@@ -18,11 +18,13 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class JwtRefreshSuccessHandler implements AuthenticationSuccessHandler {
 
-	//刷新间隔5分钟
-	private static final int tokenRefreshInterval = 300;
+	// 刷新间隔5分钟
+	private static final int TOKEN_REFRESH_INTERVAL = 300;
+
+	private static final String RESPONSE_HEADER = "Authorization";
 
 	private final JwtUserService jwtUserService;
-	
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) {
@@ -30,13 +32,13 @@ public class JwtRefreshSuccessHandler implements AuthenticationSuccessHandler {
 		boolean shouldRefresh = shouldTokenRefresh(jwt.getIssuedAt());
 		if(shouldRefresh) {
             String newToken = jwtUserService.saveUserLoginInfo((UserDetails)authentication.getPrincipal());
-            response.setHeader("Authorization", newToken);
-        }	
+            response.setHeader(RESPONSE_HEADER, newToken);
+        }
 	}
-	
+
 	private boolean shouldTokenRefresh(Date issueAt) {
         LocalDateTime issueTime = LocalDateTime.ofInstant(issueAt.toInstant(), ZoneId.systemDefault());
-        return LocalDateTime.now().minusSeconds(tokenRefreshInterval).isAfter(issueTime);
+        return LocalDateTime.now().minusSeconds(TOKEN_REFRESH_INTERVAL).isAfter(issueTime);
     }
 
 }
