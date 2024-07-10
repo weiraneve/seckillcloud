4071d586d77d6bd290b18cee58d1438985805248
weiraneve <907221539@qq.com>
2024-07-10 10:04:38 +0800

    refactor: extract static method static header writer

--- cloud-manage/src/main/java/com/weiran/manage/config/security/WebSecurityConfig.java
+++ cloud-manage/src/main/java/com/weiran/manage/config/security/WebSecurityConfig.java
@@ -26,10 +26,9 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 import java.util.Collections;
 
-
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true) // 开启方法级的安全；prePostEnabled = true开启方法执行前与后，PreAuthorize注解会在方法执行前验证。
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AdminUserMapper adminUserMapper;
@@ -40,34 +39,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/actuator/**", "/upload", "/session/findByUsername", "/login").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
+                .and().csrf().disable()
                 .formLogin().disable()
                 .sessionManagement().disable()
                 .cors()
-                .and()
-                .headers().addHeaderWriter(new StaticHeadersWriter(Arrays.asList(
-                        new Header("Access-control-Allow-Origin", "*"),
-                        new Header("Access-Control-Expose-Headers", "Authorization"))))
-                .and().exceptionHandling().accessDeniedHandler(new RestAuthenticationAccessDeniedHandler()).and()
-                .addFilterAfter(new OptionsRequestFilter(), CorsFilter.class)
+                .and().headers().addHeaderWriter(staticHeadersWriter())
+                .and().exceptionHandling().accessDeniedHandler(new RestAuthenticationAccessDeniedHandler())
+                .and().addFilterAfter(new OptionsRequestFilter(), CorsFilter.class)
                 .apply(new JsonLoginConfigurer<>()).loginSuccessHandler(jsonLoginSuccessHandler())
-                .and()
-                .apply(new JwtLoginConfigurer<>()).tokenValidSuccessHandler(jwtRefreshSuccessHandler()).permissiveRequestUrls("/logout")
-                .and()
-                .logout()
-                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
-                .and()
-                .sessionManagement().disable();
+                .and().apply(new JwtLoginConfigurer<>()).tokenValidSuccessHandler(jwtRefreshSuccessHandler()).permissiveRequestUrls("/logout")
+                .and().logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+                .and().sessionManagement().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(daoAuthenticationProvider()).authenticationProvider(jwtAuthenticationProvider());
+        auth.authenticationProvider(daoAuthenticationProvider())
+                .authenticationProvider(jwtAuthenticationProvider());
     }
 
-    @Override
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
@@ -84,7 +76,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean("daoAuthenticationProvider")
     protected AuthenticationProvider daoAuthenticationProvider() {
-        // 这里会默认使用BCryptPasswordEncoder比对加密后的密码，注意要跟createUser时保持一致
         DaoAuthenticationProvider daoProvider = new DaoAuthenticationProvider();
         daoProvider.setUserDetailsService(userDetailsService());
         daoProvider.setHideUserNotFoundExceptions(false);
@@ -116,7 +107,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "DELETE", "PATCH", "PUT", "OPTION"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "DELETE", "PATCH", "PUT", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -124,4 +115,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return source;
     }
 
+    private StaticHeadersWriter staticHeadersWriter() {
+        return new StaticHeadersWriter(Arrays.asList(
+                new Header("Access-control-Allow-Origin", "*"),
+                new Header("Access-Control-Expose-Headers", "Authorization")));
+    }
 }
