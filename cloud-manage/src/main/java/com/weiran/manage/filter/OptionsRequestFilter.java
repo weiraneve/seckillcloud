2cb9cecade2d279edabe6ffbe18b546cd39e689e
weiraneve <907221539@qq.com>
2024-08-14 10:54:29 +0800

    refactor: add not null annotation

--- cloud-manage/src/main/java/com/weiran/manage/filter/OptionsRequestFilter.java
+++ cloud-manage/src/main/java/com/weiran/manage/filter/OptionsRequestFilter.java
@@ -6,22 +6,24 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import lombok.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
-public class OptionsRequestFilter extends OncePerRequestFilter{
+public class OptionsRequestFilter extends OncePerRequestFilter {
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		if(request.getMethod().equals("OPTIONS")) {
-			response.setHeader("Access-Control-Allow-Credentials", "true");
-			response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET,DELETE,PUT,PATCH");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, Content-Language, Cache-Control, X-E4M-With");
-			return;
-		}
-		filterChain.doFilter(request, response);
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET,DELETE,PUT,PATCH");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, Content-Language, Cache-Control, X-E4M-With");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
 
 }
