6f9f0753bd4df5092018eb8bfe6316815068e8dc
weiraneve <907221539@qq.com>
2024-08-21 17:31:32 +0800

    refactor: extract option request filter code constants

--- cloud-manage/src/main/java/com/weiran/manage/filter/OptionsRequestFilter.java
+++ cloud-manage/src/main/java/com/weiran/manage/filter/OptionsRequestFilter.java
@@ -13,17 +13,26 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 public class OptionsRequestFilter extends OncePerRequestFilter {
 
+    private static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+    private static final String ALLOWED_CREDENTIALS = "true";
+    private static final String ALLOWED_METHODS = "OPTIONS,POST,GET,DELETE,PUT,PATCH";
+    private static final String MAX_AGE = "3600";
+    private static final String ALLOWED_HEADERS = "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, Content-Language, Cache-Control, X-E4M-With";
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         if (request.getMethod().equals("OPTIONS")) {
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST,GET,DELETE,PUT,PATCH");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, Content-Language, Cache-Control, X-E4M-With");
+            response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOWED_CREDENTIALS);
+            response.setHeader(ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHODS);
+            response.setHeader(ACCESS_CONTROL_MAX_AGE, MAX_AGE);
+            response.setHeader(ACCESS_CONTROL_ALLOW_HEADERS, ALLOWED_HEADERS);
             return;
         }
         filterChain.doFilter(request, response);
     }
-
 }
