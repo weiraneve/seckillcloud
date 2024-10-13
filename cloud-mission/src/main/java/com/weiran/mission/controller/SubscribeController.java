1fac7e6d00e03c5d39916f0359efc16661f1f49d
weiraneve <907221539@qq.com>
2024-10-13 09:19:54 +0800

    feat: add user subscribe controller

---
+++ cloud-mission/src/main/java/com/weiran/mission/controller/SubscribeController.java
@@ -0,0 +1,14 @@
+package com.weiran.mission.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api("用户订阅控制器")
+@RequiredArgsConstructor
+@RequestMapping("/subscribe")
+public class SubscribeController {
+
+}
