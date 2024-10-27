33ae4143f9bc984221d54b9464388fe68d7224b4
weiraneve <907221539@qq.com>
2024-10-27 10:01:22 +0800

    feat: add seckill entity content

--- cloud-mission/src/main/java/com/weiran/mission/pojo/entity/SeckillNotification.java
+++ cloud-mission/src/main/java/com/weiran/mission/pojo/entity/SeckillNotification.java
@@ -1,4 +1,44 @@
 package com.weiran.mission.pojo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@ApiModel(description = "订阅表")
+@TableName("seckill_notification")
 public class SeckillNotification {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty("主键")
+    private Long id;
+
+    @ApiModelProperty("用户id")
+    private Long userId;
+
+    @ApiModelProperty("商品id")
+    private Long goodsId;
+
+    @ApiModelProperty("秒杀时间")
+    private LocalDateTime seckillTime;
+
+    @ApiModelProperty("EMAIL/SMS")
+    private String notifyType;
+
+    @ApiModelProperty("邮箱或手机号")
+    private String notifyTarget;
+
+    @ApiModelProperty("商品id")
+    private Boolean notified = false;
+
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createdAt;
+
+    @ApiModelProperty("更新时间")
+    private LocalDateTime updatedAt;
 }
