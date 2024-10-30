9aa0460c7d0770d6cdfd32429967cfcb2071b400
weiraneve <907221539@qq.com>
2024-10-30 15:05:30 +0800

    feat: add seckill notification find by user id

--- cloud-mission/src/main/java/com/weiran/mission/mapper/SeckillNotificationMapper.java
+++ cloud-mission/src/main/java/com/weiran/mission/mapper/SeckillNotificationMapper.java
@@ -4,9 +4,16 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.weiran.mission.pojo.entity.SeckillNotification;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 @DS("goods")
 public interface SeckillNotificationMapper extends BaseMapper<SeckillNotification> {
 
+    /**
+     * 根据userId查询SeckillNotification
+     */
+    List<SeckillNotification> findSeckillNotificationByUserId(@Param("userId") Long userId);
 }
