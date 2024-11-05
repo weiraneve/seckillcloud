da0b9db292f4892b070fe71dcce1399e14c19dca
weiraneve <907221539@qq.com>
2024-11-05 17:34:35 +0800

    feat: add notification mapper dependency

--- cloud-mission/src/main/java/com/weiran/mission/service/impl/NotificationServiceImpl.java
+++ cloud-mission/src/main/java/com/weiran/mission/service/impl/NotificationServiceImpl.java
@@ -1,13 +1,21 @@
 package com.weiran.mission.service.impl;
 
+import com.weiran.mission.mapper.SeckillNotificationMapper;
 import com.weiran.mission.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
     private final int LOOP_CHECK_INTERVAL = 60000;
+
+    private final SeckillNotificationMapper seckillNotificationMapper;
 
     @Override
     public void createNotification(Long userId, Long goodsId) {
