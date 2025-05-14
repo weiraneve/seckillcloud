c1852ba59b01dbe9bb7144f508a56d8845458233
weiraneve <907221539@qq.com>
2025-05-14 15:59:08 +0800

    feat: add notification cloud interface

--- cloud-manage/src/main/java/com/weiran/manage/cloud/MissionClient.java
+++ cloud-manage/src/main/java/com/weiran/manage/cloud/MissionClient.java
@@ -5,19 +5,13 @@ import com.weiran.common.obj.Result;
 import com.weiran.common.pojo.dto.GoodsDTO;
 import com.weiran.common.pojo.dto.OrderDTO;
 import com.weiran.common.pojo.dto.SeckillGoodsDTO;
+import com.weiran.common.pojo.dto.SeckillNotificationDTO;
 import com.weiran.common.pojo.vo.WelcomeVO;
 import com.weiran.manage.cloud.fallback.MissionClientFallback;
 import com.weiran.manage.config.FeignInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -58,6 +52,7 @@ public interface MissionClient {
     @GetMapping("/cloud/order/findByOrders")
     Result<PageInfo<OrderDTO>> findByOrders(@RequestParam(value = "page", defaultValue = "1") Integer page,
                                             @RequestParam(value = "pageSize", defaultValue = "1") Integer pageSize, @RequestParam(required = false) Long id);
+
     // 分页查询秒杀商品
     @GetMapping("/cloud/seckillGoods")
     Result<PageInfo<SeckillGoodsDTO>> seckillGoods(@RequestParam(value = "page", defaultValue = "1") Integer page,
@@ -66,17 +61,21 @@ public interface MissionClient {
     // 统计查询
     @GetMapping("/cloud/welcome")
     Result<WelcomeVO> welcome();
-    
+
     // 上传图片
     @PostMapping(value = "/cloud/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Result<Object> uploadPic(@RequestPart("file") MultipartFile file, @RequestParam("type") Integer type);
-        
+
     // 删除图片
     @DeleteMapping("/cloud/upload")
     Result<Object> deletePic(@RequestParam("key") String key);
-    
+
     // 批量删除图片
     @DeleteMapping("/cloud/upload/deletes")
     Result<Object> deletesPic(@RequestParam("keys") String[] keys);
-    
+
+    // 分页查询通知
+    @GetMapping("/cloud/notification")
+    Result<PageInfo<SeckillNotificationDTO>> notificationIndex(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "pageSize", defaultValue = "1") Integer pageSize,
+                                                               @RequestParam(required = false) String goodsName);
 }
--- cloud-manage/src/main/java/com/weiran/manage/controller/web/NotificationController.java
+++ cloud-manage/src/main/java/com/weiran/manage/controller/web/NotificationController.java
@@ -1,10 +1,16 @@
 package com.weiran.manage.controller.web;
 
 
+import com.github.pagehelper.PageInfo;
+import com.weiran.common.obj.Result;
+import com.weiran.common.pojo.dto.SeckillNotificationDTO;
 import com.weiran.manage.cloud.MissionClient;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("通知列表")
@@ -14,5 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
     private final MissionClient missionClient;
 
+    @ApiOperation("分页查询通知")
+    @GetMapping
+    public Result<PageInfo<SeckillNotificationDTO>> goodsIndex(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "pageSize", defaultValue = "1") Integer pageSize,
+                                                               @RequestParam(required = false) String goodsName) {
+        return missionClient.notificationIndex(page, pageSize, goodsName);
+    }
 
 }
